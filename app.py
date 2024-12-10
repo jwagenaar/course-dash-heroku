9fc94c30731e80e1abb07e9e5a2d77c4762a6d1a
Joost Wagenaar <1605953+jwagenaar@users.noreply.github.com>
2024-12-10 12:45:25 -0800

    pennsieve agent

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
+import logging
+
 from dash import Dash, html, dcc, callback, Output, Input
 import plotly.express as px
 import pandas as pd
+import pennsieve2 as ps
 
 df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/gapminder_unfiltered.csv')
 
@@ -8,8 +11,20 @@ app = Dash()
 
 server = app.server
 
+ps = ps.Pennsieve(connect=True)
+
+session = ps.connect()
+logging.info(session)
+
+user = session.get_user()
+logging.info(user)
+# ds = ps.get('https://api.pennsieve.io/discover/datasets', params={'limit':20})
+# df_list = pd.DataFrame(ds['datasets'])
+
+
 app.layout = [
     html.H1(children='Title of Dash App', style={'textAlign':'center'}),
+    # dcc.Dropdown(df_list.name.unique(), '' , id='dropdown-selection' ),
     dcc.Dropdown(df.country.unique(), 'Canada', id='dropdown-selection'),
     dcc.Graph(id='graph-content')
 ]
